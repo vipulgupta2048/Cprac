6d67ba43fe30ba83a559075730dbb75f58f8c156
Vipul Gupta <vipulgupta2048@gmail.com>
2017-10-10 22:55:02 +0530

    array and char array

--- TUT/PYthon/array-list.py
+++ TUT/PYthon/array-list.py
@@ -2,4 +2,15 @@ my_integers = [5, 7, 1, 3, 4]
 print(my_integers[0]) # 5
 print(my_integers[1]) # 7
 print(my_integers[4]) # 4
+print("")
 
+relative =["boy", "foo", "sho", "loo", "goo"]
+print(relative[3]) #loo
+print("")
+
+foo = []
+foo.append("boy that's weird")
+foo.append("what is ?")
+foo.append("something sure is")
+print(foo[1])
+print(foo[2])
