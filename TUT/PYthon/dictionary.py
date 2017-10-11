55157870b60b24f147ce07d31880e0bf02057768
Vipul Gupta <vipulgupta2048@gmail.com>
2017-10-11 08:05:27 +0530

    dictionary_1

---
+++ TUT/PYthon/dictionary.py
@@ -0,0 +1,10 @@
+dictionary_vip = {
+	"name" 		: "Vipul Gupta",
+	"nickname" 	: "lupiv",
+	"country" 	: "India"
+}
+
+dictionary_vip['age'] = 24
+
+print(dictionary_vip)
+print(" the name of the person is %s" %(dictionary_vip["name"]))
