2c53e0a4659c4fb459ed093de273e9982c292975
Vipul Gupta <vipulgupta2048@gmail.com>
2018-04-12 20:16:51 +0530

    Login with swing

---
+++ TUT/java/loginwithswing.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+public class loginwithswing {
+	public static void main (String args[]) {
+        JFrame frame = new JFrame("First");
+        frame.setSize(350,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placecomponets(panel);
+        frame.setVisible(true);
+    }
+
+    public static void placecomponets(JPanel panel){
+        panel.setLayout(null);
+
+        JLabel userlabel = new JLabel("User");
+        userlabel.setBounds(10,20,80,25);
+        panel.add(userlabel);
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+    }
+}
+
