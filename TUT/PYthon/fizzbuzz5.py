64d4763a11aa179ff6b1251c870d6bd391ec44f1
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2019-08-30 01:19:25 +0530

    More Fizzbuzz
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

---
+++ TUT/PYthon/fizzbuzz5.py
@@ -0,0 +1,17 @@
+# Write a program that prints the numbers from 1 to 100 and for multiples of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buzz".
+
+def voice(i):
+    switcher = {
+        i % 15 : a.append("Fizzbuzz"),
+        i % 3 : a.append("Fizz"),
+        i % 5 : a.append("Buzz")
+    }
+    return switcher.get(i, a.append(i))
+
+
+a=[]
+for i in range(0, 101):
+    a.append(voice(i))
+
+for y in a:
+    print(y, end="  ")
