4edc72a8fbc112fc3949cb316bb32b91b0dc0202
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2019-09-03 13:14:54 +0530

    Add Stars practice problems
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- TUT/PYthon/stars.py
+++ TUT/PYthon/stars.py
@@ -1,65 +1,86 @@
-# ~ for i in range(0, n):
+# Make stars like this,
 
-        # ~ # inner loop to handle number of columns
-        # ~ # values changing acc. to outer loop
-        # ~ for j in range(0, i+1):
+# *
+# * *
+# * * *
+# * * * *
 
-            # ~ # printing stars
-            # ~ print("* ",end="")
-
-        # ~ # ending line after each row
-        # ~ print("\r")
 n=4
+for i in range(0, n):
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+            # printing stars
+            print("* ",end="")
+        # ending line after each row
+        print("\r")
+print("\r")
+
+# -----------------------------------------------------------------
 
 # Python 3.x code to demonstrate star pattern
 
+#       *
+#     * *
+#   * * *
+# * * * *
+
 # Function to demonstrate printing pattern
-# ~ def pypart2(n):
+def pypart2(n):
+    # number of spaces
+    k = 2*n - 2
 
-# number of spaces
-# ~ k = 2*n - 2
+    # outer loop to handle number of rows
+    for i in range(0, n):
 
-# ~ # outer loop to handle number of rows
-# ~ for i in range(0, n):
+        # inner loop to handle number spaces
+        # values changing acc. to requirement
+        for j in range(0, k):
+            print(end=" ")
 
-    # ~ # inner loop to handle number spaces
-    # ~ # values changing acc. to requirement
-    # ~ for j in range(0, k):
-        # ~ print(end=" ")
+        # decrementing k after each loop
+        k = k - 2
 
-    # ~ # decrementing k after each loop
-    # ~ k = k - 2
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
 
-    # ~ # inner loop to handle number of columns
-    # ~ # values changing acc. to outer loop
-    # ~ for j in range(0, i+1):
+            # printing stars
+            print("* ", end="")
 
-        # ~ # printing stars
-        # ~ print("* ", end="")
+        # ending line after each row
+        print("\r")
 
-    # ~ # ending line after each row
-    # ~ print("\r")
+pypart2(n)
+print("\r")
 
+# ----------------------------------------------------------------------
 
-# ~ k = 2*n - 2
+#    *
+#   * *
+#  * * *
+# * * * *
 
-# ~ # outer loop to handle number of rows
-# ~ for i in range(0, n):
+k = 2*n - 2
 
-    # ~ # inner loop to handle number spaces
-    # ~ # values changing acc. to requirement
-    # ~ for j in range(0, k):
-        # ~ print(end=" ")
+# # outer loop to handle number of rows
+for i in range(0, n):
 
-    # ~ # decrementing k after each loop
-    # ~ k = k - 1
+    # inner loop to handle number spaces
+    # values changing acc. to requirement
+    for j in range(0, k):
+        print(end=" ")
 
-    # ~ # inner loop to handle number of columns
-    # ~ # values changing acc. to outer loop
-    # ~ for j in range(0, i+1):
+    # decrementing k after each loop
+    k = k - 1
 
-        # ~ # printing stars
-        # ~ print("* ", end="")
+    # inner loop to handle number of columns
+    # values changing acc. to outer loop
+    for j in range(0, i+1):
 
-    # ~ # ending line after each row
-    # ~ print("\r")
+        # printing stars
+        print("* ", end="")
+
+    # ending line after each row
+    print("\r")
+print("\r")
